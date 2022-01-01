657bdf939e93e5b2ad64e943cd689b3f91db502b
Doyss <47415216+Doyss@users.noreply.github.com>
2022-01-01 11:36:15 +0300

    2

--- main.go
+++ main.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func home_page(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Go is...")
+}
+func contacts_page(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "is contacts")
+}
+
+func handleRequerst(){
+	http.HandleFunc("/", home_page)	
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main(){
-	fmt.Println("Hello world!")
+	handleRequerst()
 }
